ca30ca9621d7a56b9defbf4f5e82894b8a64b985
Kumari Khushi Singh <157168126+KhushiSingh0823@users.noreply.github.com>
2024-08-26 07:26:59 +0530

    Update PostOrderTraversal.java

--- PostOrderTraversal.java
+++ PostOrderTraversal.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 public class PostOrderTraversal {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postOrder(root, list);
+        return list;
     }
 
     public void postOrder(TreeNode root, List<Integer> list){
@@ -19,25 +19,17 @@ public class PostOrderTraversal {
         postOrder(root.left, list);
         postOrder(root.right, list);
         list.add(root.val);
-
-    }
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        postOrder(root, list);
-        return list;
     }
 
     public static void main(String[] args) {
-        PostOrderTraversal postOrderTraversal = new PostOrderTraversal();
+        PostOrderTraversal traversal = new PostOrderTraversal();
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
-        root.right.left = new TreeNode(6);
-        root.right.right = new TreeNode(7);
 
-        List<Integer> result = postOrderTraversal.postorderTraversal(root);
-        System.out.println("Post-order traversal: " + result);
+        List<Integer> result = traversal.postorderTraversal(root);
+        System.out.println("Postorder Traversal: " + result);
     }
 }
